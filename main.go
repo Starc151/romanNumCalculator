885dd9cf2bc5c6e953d87105ad3eda72a6e59d07
Aleksey Pashkov <dr.Starc@ya.ru>
2022-12-15 17:59:48 +0300

    processing...

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
-	"calc"
-	// "convertNum"
+	// "calc"
+	cN "convertNum"
 	"fmt"
 	"os"
 	"strconv"
@@ -17,35 +17,49 @@ func inputUser() []string {
 	mathExpression := strings.Split(in.Text(), " ")
 	return mathExpression
 }
-func arabOrRom(aStr, bStr string) (a, b int) {
-	a, errA := strconv.Atoi(aStr)
-	b, errB := strconv.Atoi(bStr)
-	if errA == nil && errB == nil {
-		return a, b
+func arabOrRom(str string) (num int, typeNum string) {
+	num, err := strconv.Atoi(str)
+	if err == nil {
+		typeNum = "arabic"
+		return num, typeNum
 	}
-	return 100, 100
+	if err != nil {
+		num = cN.ToArabic(str)
+		typeNum = "rom"
+		return num, typeNum
+	}
+	return
 }
 func main() {
-	mathExpression := inputUser()
-	aStr := mathExpression[0]
-	bStr := mathExpression[2]
-	a, b := arabOrRom(aStr, bStr)
-	sign := mathExpression[1]
-	if (a < 1 || a > 10) || (b < 1 || b > 10) {
-		fmt.Println("Одно из чисел не в допустимом диапазоне")
-		return
-	}
-	signs := [4]string{"+", "-", "*", "/"}
-	signsBool := false
-	for _, v := range signs {
-		if sign == v {
-			signsBool = true
-			break
-		}
-	}
-	if !signsBool {
-		fmt.Println("Нет такой мат.операции")
-		return
-	}
-	fmt.Println(calc.Calc(a, b, sign))
+	fmt.Println(arabOrRom("G"))
+	// mathExpression := inputUser()
+	// if len(mathExpression) < 3 {
+	// 	fmt.Println("Строка не является математической операцией!")
+	// 	return
+	// }
+	// if len(mathExpression) > 3 {
+	// 	fmt.Println("Формат мат.операции не удовлетворяет заданию")
+	// 	return
+	// }
+	// aStr := mathExpression[0]
+	// bStr := mathExpression[2]
+	// a, b := arabOrRom(aStr, bStr)
+	// sign := mathExpression[1]
+	// if (a < 1 || a > 10) || (b < 1 || b > 10) {
+	// 	fmt.Println("Одно из чисел не в допустимом диапазоне")
+	// 	return
+	// }
+	// signs := [4]string{"+", "-", "*", "/"}
+	// signsBool := false
+	// for _, v := range signs {
+	// 	if sign == v {
+	// 		signsBool = true
+	// 		break
+	// 	}
+	// }
+	// if !signsBool {
+	// 	fmt.Println("Нет такой мат.операции")
+	// 	return
+	// }
+	// fmt.Println(calc.Calc(a, b, sign))
 }
